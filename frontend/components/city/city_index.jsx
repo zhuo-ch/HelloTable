452df7ece56d94e692f79d22f19f79dc76d76d14
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-05-17 11:24:56 -0400

    Fix login hook for restaurant create

--- frontend/components/city/city_index.jsx
+++ frontend/components/city/city_index.jsx
@@ -17,7 +17,7 @@ class CityIndex extends React.Component {
   }
 
   handleCreate() {
-    if (this.currentUser) {
+    if (this.props.currentUser) {
       this.props.setCurrentModal({hidden: false, type: 'create'});
     } else {
       this.props.setCurrentModal({hidden: false, type: 'login'});
