3ff4d46e5c6a1beb37bd0bcd8a2cd9418510fb57
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-05-17 23:01:12 -0400

    Add footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,11 +6,24 @@ import Modal from './modal';
 
 const App = (props) => {
 
+  const Footer = () => {
+    return (
+      <footer className="footer">
+        <ul className="footer-links">
+          <li><a href="https://zhuo-ch.github.io/" className="copyright"><span className="label">&copy; Zhuo Chen</span></a></li>
+          <li><a href="https://www.linkedin.com/in/zhuohchen/" className="fa fa-linkedin"> <span className="label">LinkedIn</span></a></li>
+          <li><a href="https://github.com/zhuo-ch/" className="fa fa-github"> <span className="label">GitHub</span></a></li>
+        </ul>
+      </footer>
+    )
+  }
+
   return (
     <div className='main-app'>
       <Modal />
       <Navbar />
       { props.children }
+      <Footer />
     </div>
   )
 }
