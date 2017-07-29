6c3942fc1dd7fc2d1538de94ba3f3338551ac8b7
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-07-29 17:01:27 -0400

    Add clear errors when resetting form

--- frontend/actions/modal_actions.js
+++ frontend/actions/modal_actions.js
@@ -1,19 +1,26 @@
 export const RECEIVE_CURRENT_MODAL = 'RECEIVE_CURRENT_MODAL';
 export const RECEIVE_RESET_MODAL = 'RECEIVE_RESET_MODAL';
+const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
 export const setCurrentModal = modal => dispatch => {
   dispatch(receiveCurrentModal(modal));
 }
 
 export const resetCurrentModal = () => dispatch => {
+  dispatch(receiveErrors([]));
   dispatch(receiveResetModal());
 }
 
 const receiveCurrentModal = modal => ({
   type: RECEIVE_CURRENT_MODAL,
   modal,
-})
+});
 
 const receiveResetModal = () => ({
   type: RECEIVE_RESET_MODAL,
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
 })
