cb7b1ffbd262572ad419e3c5c2dbdbac2cf4a762
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-08-02 15:53:16 -0400

    Add production config to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,10 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
