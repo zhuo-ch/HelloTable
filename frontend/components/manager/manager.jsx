f3334ae775998c8eaa13e160a34fec2341315bd6
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-25 23:46:41 -0400

    Add manager.jsx.

---
+++ frontend/components/manager/manager.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Manager extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
