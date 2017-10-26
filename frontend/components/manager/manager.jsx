8b4b9985d25aef22d2d37bd68d63d07956667020
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-26 00:35:26 -0400

    Add display methods.

--- frontend/components/manager/manager.jsx
+++ frontend/components/manager/manager.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Modal from '../modal.jsx';
+
 class Manager extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  getDetails() {
+
+  }
+
+  getTimes() {
+
+  }
+
+  getLimits() {
+
   }
 
   render() {
