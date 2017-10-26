08c7fb618f92b44cf8ed901f690504c6d2da1c77
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-10-26 00:32:40 -0400

    Import modal.

--- frontend/components/manager/manager.jsx
+++ frontend/components/manager/manager.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import Modal from '../modal.jsx';
 class Manager extends React.Component {
   constructor(props) {
     super(props);
