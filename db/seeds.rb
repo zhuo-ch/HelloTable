c6e292c3c6fb890b583fd479c5207f47dce63158
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-12-12 19:59:00 -0500

    Add seeding for upcoming reservations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,9 +50,6 @@ def generate_restaurant(restaurant, city_id, cuisines, descriptions, user)
     location: restaurant["geometry"]["location"]
     )
 
-    if !res.id
-      debugger
-    end
   generate_hours(res.id)
   generate_seating(res.id)
 
@@ -160,10 +157,18 @@ end
   id = restaurants.sample
   seating = Restaurant.find(id).seatings.sample
   Reservation.create(user_id: guest.id, restaurant_id: id,
-    date: "#{rand(3..11)}-" + "#{rand(1..30)}" + "-2017", time: times.sample, seating_id: seating.id, seats: seating.seats)
+    date: "#{rand(3..11)}-" + "#{rand(1..30)}" + "-2017", time: times.sample,
+    seating_id: seating.id, seats: seating.seats)
 end
 
 # set upcoming reservations
+
+30.times do |i|
+  seating = Restaurant.last.seatings.sample
+  Reservation.create(user_id: user, restaurant_id: Restaurant.last.id,
+    date: "12-#{i + 1}-2017", time: times.sample, seating_id: seating.id,
+    seats: seating.seats)
+end
 
 5.times do
   id = restaurants.sample
