18329eff811e6c4681012f6eb68f1c74dbb1c556
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-12-14 18:19:08 -0500

    Fix time for 24 in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -126,7 +126,7 @@ Restaurant.all.each do |restaurant|
   5.times do
     seating = available_seats.sample
     Reservation.create(user_id: user, restaurant_id: restaurant.id,
-      date: "#{12}-#{rand(1..30)}-2017", time: times.sample + 12, seating_id: seating.id, seats: seating.seats)
+      date: "#{12}-#{rand(1..30)}-2017", time: times.sample + 1200, seating_id: seating.id, seats: seating.seats)
   end
 end
 
@@ -157,7 +157,7 @@ end
   id = restaurants.sample
   seating = Restaurant.find(id).seatings.sample
   Reservation.create(user_id: guest.id, restaurant_id: id,
-    date: "#{rand(3..11)}-" + "#{rand(1..30)}" + "-2017", time: times.sample + 12,
+    date: "#{rand(3..11)}-" + "#{rand(1..30)}" + "-2017", time: times.sample + 1200,
     seating_id: seating.id, seats: seating.seats)
 end
 
@@ -166,13 +166,13 @@ end
 30.times do |i|
   seating = Restaurant.last.seatings.sample
   Reservation.create(user_id: user, restaurant_id: Restaurant.last.id,
-    date: "12-#{i + 1}-2017", time: times.sample + 12, seating_id: seating.id,
+    date: "12-#{i + 1}-2017", time: times.sample + 1200, seating_id: seating.id,
     seats: seating.seats)
 end
 
-5.times do
+3.times do
   id = restaurants.sample
   seating = Restaurant.find(id).seatings.sample
   Reservation.create(user_id: guest.id, restaurant_id: id,
-    date: "#{12}-" + "#{rand(1..30)}" + "-2017", time: times.sample + 12, seating_id: seating.id, seats: seating.seats)
+    date: "#{12}-" + "#{rand(15..30)}" + "-2017", time: times.sample + 1200, seating_id: seating.id, seats: seating.seats)
 end
