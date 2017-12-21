55922c36c9982d4988e2b47c207a89eaca915f2f
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-12-21 18:15:31 -0500

    Add current_session to render window.current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :current_session, :logged_in?
 
   def current_user
     user = User.find_by(session_token: session[:session_token])
+  end
+
+  def current_session
+    user = User.find_session(session[:session_token])
   end
 
   def logged_in?
