204953fe810dec8331fb4720fa62296e0268d59b
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-12-23 19:13:36 -0500

    Finalize componentization. Remove dup functions.

--- frontend/components/manager/manager.jsx
+++ frontend/components/manager/manager.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, hashHistory } from 'react-router';
-import { setCurrentModal, resetCurrentModal } from '../../actions/modal_actions';
-import {
-  fetchManagerRestaurant,
-  updateRestaurant,
-  setError,
-  clearErrors,
-} from '../../actions/manager_actions';
+import { fetchManagerRestaurant } from '../../actions/manager_actions';
 import { resetRestaurant } from '../../actions/restaurant_actions';
-import { formatDate } from '../../util/search_util';
+import { resetReservation } from '../../actions/reservations_actions';
 import { StickyContainer, Sticky } from 'react-sticky';
 import ManagerSideBar from './manager_side_bar';
 import RestaurantMap from '../restaurant/restaurant_map';
@@ -27,9 +21,6 @@ class Manager extends React.Component {
     super(props);
     this.state = { selecting: false, idx: '', value: '', };
     this.handleSideBar = this.handleSideBar.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSave = this.handleSave.bind(this);
   }
 
   componentWillMount() {
@@ -38,11 +29,6 @@ class Manager extends React.Component {
     } else {
       this.props.fetchManagerRestaurant(this.props.currentUser.id);
     }
-  }
-
-  componentDidMount() {
-    const date = formatDate();
-    this.setState({ date });
   }
 
   componentWillUnmount() {
@@ -56,36 +42,6 @@ class Manager extends React.Component {
     reference.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
-  handleClick(e) {
-    e ? e.preventDefault() : '';
-    this.props.clearErrors();
-    setTimeout(this.props.resetCurrentModal, 300);
-    const selecting = this.state.selecting ? false : true;
-    this.setState({ selecting: selecting });
-    e ? this.setState({ idx: e.currentTarget.id }) : '';
-  }
-
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({ value: e.currentTarget.value });
-  }
-
-  handleSave() {
-    const idx = this.state.idx.split('-');
-    this.props.setCurrentModal({ hidden: false, type: 'spinner' });
-
-    switch (idx[0]) {
-      case 'hours':
-        this.handleUpdateHour(idx);
-        break;
-      case 'seatings':
-        this.handleUpdateSeating(idx);
-        break;
-      default:
-        this.handleUpdateRestaurant(idx)
-    }
-  }
-
   getSideBar() {
     return (
       <ManagerSideBar
@@ -95,19 +51,8 @@ class Manager extends React.Component {
     );
   }
 
-  getRightBar() {
-    return (
-      <section className='manager-map'>
-        <RestaurantMap
-          location={ this.props.restaurant.location }
-          address={ this.props.restaurant.address } />
-      </section>
-    );
-  }
-
   render() {
     const sideBar = this.getSideBar();
-    const rightBar = this.getRightBar();
 
     return (
       <StickyContainer className='restaurant-view'>
@@ -123,7 +68,9 @@ class Manager extends React.Component {
             <ManagerReviews />
           </div>
           <div className='restaurant-right'>
-            { rightBar }
+            <section className='manager-map'>
+              <RestaurantMap />
+            </section>
           </div>
         </div>
       </StickyContainer>
@@ -137,12 +84,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  resetCurrentModal: () => dispatch(resetCurrentModal()),
-  setCurrentModal: modal => dispatch(setCurrentModal(modal)),
   fetchManagerRestaurant: id => dispatch(fetchManagerRestaurant(id)),
   resetRestaurant: () => dispatch(resetRestaurant()),
-  updateRestaurant: restaurant => dispatch(updateRestaurant(restaurant)),
-  clearErrors: () => dispatch(clearErrors()),
+  resetReservation: () => dispatch(resetReservation()),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Manager));
