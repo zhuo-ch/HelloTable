45ce4fdd3fe34a667886933ee6dc10278184e24b
Zhuo Chen <zhuo.h.chen@gmail.com>
2017-12-29 23:05:47 -0500

    Change cityactions to use restaurant reducer.

--- frontend/actions/city_actions.js
+++ frontend/actions/city_actions.js
@@ -1,7 +1,8 @@
 import * as CityAPIUtil from '../util/city_api_util';
 
 export const RECEIVE_ALL_CITIES = 'RECEIVE_ALL_CITIES';
-export const RECEIVE_CITY = 'RECEIVE_CITY';
+export const RECEIVE_ALL_RESTAURANTS = 'RECEIVE_ALL_RESTAURANTS';
+// export const RECEIVE_CITY = 'RECEIVE_CITY';
 
 export const fetchAllCities = () => dispatch => {
   return CityAPIUtil.fetchAllCities()
@@ -10,15 +11,19 @@ export const fetchAllCities = () => dispatch => {
 
 export const fetchCity = cityId => dispatch => {
   return CityAPIUtil.fetchCity(cityId)
-    .then((city) => dispatch(receiveCity(city)));
+    .then((restaurants) => dispatch(receiveAllRestaurants(restaurants)));
 }
+// export const fetchCity = cityId => dispatch => {
+//   return CityAPIUtil.fetchCity(cityId)
+//     .then((city) => dispatch(receiveCity(city)));
+// }
 
 const receiveAllCities = cities => ({
   type: RECEIVE_ALL_CITIES,
   cities,
-})
+});
 
-const receiveCity = city => ({
-  type: RECEIVE_CITY,
-  city,
-})
+const receiveAllRestaurants = restaurants => ({
+  type: RECEIVE_ALL_RESTAURANTS,
+  restaurants,
+});
