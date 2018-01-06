9ab3d561e6182159de5a0f2f5ebddb89b675b506
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-01-05 22:44:56 -0500

    Add UserReducer. Add receive_user, reset_user.

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,18 @@
+import { merge } from 'lodash';
+import { RECEIVE_USER } from './actions/user_actions';
+
+const _nullUser = Object.freeze({});
+
+const UserReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_USER:
+      return merge({}, state, action.user);
+    case RESET_USER:
+      return _nullUser;
+    default:
+      return state;
+  }
+}
+
+export default UserReducer;
