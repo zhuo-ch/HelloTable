7baf0d0a8d8ef0d9bc8b3092799031e746bfc123
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-01-19 10:33:47 -0500

    Fix date_selectors import date_util.timeToString.

--- frontend/selectors/date_selectors.js
+++ frontend/selectors/date_selectors.js
@@ -2,7 +2,7 @@ import * as DateUtil from '../util/date_util';
 
 export const setUpcoming = reservation => {
   const curDate = new Date();
-  const newDate = new Date(reservation.date + ' ' + timeToString(reservation.time));
+  const newDate = new Date(reservation.date + ' ' + DateUtil.timeToString(reservation.time));
 
   return newDate > curDate;
 }
